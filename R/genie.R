519ba5db0f7722840b9481f44f6f4b2229d7c092
Julie Laffy <julie.laffy@weizmann.ac.il>
2018-06-02 22:12:05 +0300

    Minor changes to data

--- R/genie.R
+++ R/genie.R
@@ -14,7 +14,10 @@ genie <- function(key = genie::gbm_default,
                   df = genie::gbm_data,
                   hashtable = genie::gbm_hashtable) {
 
+  if (any(key == 'all')) {
+	return(df$gene[!duplicated(df$gene)])
+  }
+
   retrieve(value = hashtable[key], df = df)
 
 }
-
